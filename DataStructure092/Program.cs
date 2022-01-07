62a7cbdd68868f6c7e42d21503627f1119a1e60d
madhurya <madhuryar017@gmail.com>
2022-01-07 13:47:32 +0530

    Adding linked list

---
+++ DataStructure092/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructure092
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Add(80);
+
+            //list.InsertAtParticularPosition(2,30);
+            list.Display();
+            Console.ReadKey();
+        }
+    }
+}
